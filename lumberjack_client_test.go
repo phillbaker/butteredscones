c26dde8e65fd41e33867cd2f3a5ba404a36ec3f5
Andy Lindeman <andy@lindeman.io>
2014-10-24 09:29:43 -0400

    Tests that the client reconnects

--- lumberjack_client_test.go
+++ lumberjack_client_test.go
@@ -47,3 +47,52 @@ func TestClientSmokeTest(t *testing.T) {
 		t.Fatal("Timeout waiting for lines to arrive")
 	}
 }
+
+func TestClientReconnectSmokeTest(t *testing.T) {
+	server, err := newLumberjackServer(&serverOptions{
+		Network: "tcp",
+		Address: "127.0.0.1:0", // random port
+
+		WriteTimeout: 2 * time.Second,
+		ReadTimeout:  2 * time.Second,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+
+	// Without the server accepting connections, we should run into a connection
+	// timeout
+	client := NewLumberjackClient(&LumberjackClientOptions{
+		Network:           "tcp",
+		Address:           server.Addr().String(),
+		ConnectionTimeout: 1 * time.Second,
+		WriteTimeout:      1 * time.Second,
+		ReadTimeout:       1 * time.Second,
+	})
+
+	lines := []Data{
+		Data{"line": "foo bar baz", "offset": "25"},
+	}
+	err = client.Send(lines)
+	if err == nil {
+		t.Fatalf("Expected Send to timeout, but did not")
+	}
+
+	// Now, setup the server properly, things should go through
+	dataCh := make(chan Data, 1)
+	go server.ServeInto(dataCh)
+
+	err = client.Send(lines)
+	if err != nil {
+		t.Error(err)
+	}
+
+	select {
+	case <-dataCh:
+		// success
+	case <-time.After(250 * time.Millisecond):
+		t.Fatal("Timeout waiting for lines to arrive")
+	}
+}
